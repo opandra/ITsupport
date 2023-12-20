7b7cb9ad6dd3a4fcfd3f450744ddb64067eebc58
opandra <152113418+opandra@users.noreply.github.com>
2023-12-20 13:50:27 +1100

    netdrive.bat

--- netdrive.bat
+++ netdrive.bat
@@ -1,5 +1,5 @@
 start D:\drivers\network\sp142693.exe         ::Running the drive from the location on my usb
-timeout /t 10                                 ::Timer so I have time to complete the wizard
+timeout /t 10                                 ::Timer so you can complete the wizard
 echo "complete"                               ::Complete just for looks
 
 start D:\drivers\network\sp149315.exe
@@ -13,7 +13,7 @@ echo "complete"
 start D:\drivers\network\sp142400.exe
 echo "press any key to restart"
 timeout /t 10
-shutdown -p                                  ::basically will restart the computer, so the drivers can start
+shutdown -p                                  ::basically will restart the computer, so the drivers can run
 
 
 
