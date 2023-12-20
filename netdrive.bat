045d4eb24fd3dd03832a35fba3aaf11e17251ece
opandra <152113418+opandra@users.noreply.github.com>
2023-12-20 11:13:08 +1100

    Add files via upload

---
+++ netdrive.bat
@@ -0,0 +1,19 @@
+start D:\drivers\network\sp142693.exe
+timeout /t 30
+echo "complete"
+
+start D:\drivers\network\sp149315.exe
+timeout /t 30
+echo "complete"
+
+start D:\drivers\network\sp145803.exe
+timeout /t 30
+echo "complete"
+
+start D:\drivers\network\sp142400.exe
+echo "press any key to restart"
+timeout /t 30
+shutdown -p
+
+
+
