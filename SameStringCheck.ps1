84d991d37227780dbf29f155d6ae2966981c7ff3
opandra <152113418+opandra@users.noreply.github.com>
2023-12-21 16:00:48 +1100

    Add files via upload

---
+++ SameStringCheck.ps1
@@ -0,0 +1,8 @@
+$string1 = "String"
+$string2 = "String"
+
+if ($string1 -eq $string2) {
+    Write-Output "The strings are identical."
+} else {
+    Write-Output "The strings are not identical."
+}
